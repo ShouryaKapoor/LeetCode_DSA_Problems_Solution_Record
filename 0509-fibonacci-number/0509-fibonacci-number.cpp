4c5e9f1ad15a9a2c444faf30268d4fe6b5c9d472
Shourya Kapoor <112488155+ShouryaKapoor@users.noreply.github.com>
2025-03-21 10:15:12 +0530

    Time: 0 ms (100%), Space: 7.7 MB (86.87%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
+int dp[31];
     int fib(int n) {
-        if(n==0) return 0;
-        if(n>2)
-        {
-           return fib(n-1) + fib(n-2);
-        }
-        return 1;
+
+        if(n<=1) return n;
+        if(dp[n] != 0) return dp[n];
+ 
+        return dp[n] = fib(n-1) + fib(n-2);
     }
 };
